d911e820a106282911d1eb3448100a3b60b494b6
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-02-14 14:11:22 +0600

    find array element

---
+++ array-exists.js
@@ -0,0 +1,12 @@
+function megaFriends(friends){
+    if(Array.isArray(friends)==false){
+        return "plase enter an array";
+    }
+}
+const array = ['a','b','v','d'];
+if(array.includes('b')){
+    // console.log('b is hear');
+}
+if(array.indexOf('b') != -1){
+    console.log('b is hear');
+}
--- string-slice.js
+++ string-slice.js
@@ -7,7 +7,14 @@ console.log(words); */
 const wordsA =  sundor.split('a');
 const smallSlice=sundor.slice(5,13);
 console.log(smallSlice); */
-//
-const sundor = 'Sundarbans is a mangrove area';
+//substr
+/* const sundor = 'Sundarbans is a mangrove area';
 const sub = sundor.substr(5,5)
-console.log(sub);
+console.log(sub); */
+/* //concat
+const fullName = 'shuvo'.concat('Ghosh').concat('Babu');
+console.log(fullName); */
+// join
+const array = ['a','b','c','n']
+const allword = array.join(' ');
+console.log(allword);
