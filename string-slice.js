9e32a9a853709d346685cdf4dedb1d7ce117d2fa
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-02-14 12:51:52 +0600

    use string sclic

---
+++ string-slice.js
@@ -0,0 +1,13 @@
+/* const sundor = 'Sundarbans is a mangrove area';
+const words =  sundor.split(' ');
+const wordsA =  sundor.split('a');
+console.log(words); */
+//slice
+/* const sundor = 'Sundarbans is a mangrove area';
+const wordsA =  sundor.split('a');
+const smallSlice=sundor.slice(5,13);
+console.log(smallSlice); */
+//
+const sundor = 'Sundarbans is a mangrove area';
+const sub = sundor.substr(5,5)
+console.log(sub);
